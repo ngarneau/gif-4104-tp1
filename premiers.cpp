16a698135d0d87d1713116dfce8a6a902658646e
Nicolas Garneau <ngarneau@ngarneau.com>
2014-01-30 12:58:16 -0800

    Remove prin

--- premiers.cpp
+++ premiers.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 
     // Afficher les nombres trouvés à la console
     for (unsigned long p=2; p<lMax; p++) {
-        if (lFlags[p] == 0) printf("%ld ", p);
+        //if (lFlags[p] == 0) printf("%ld ", p);
     }
     printf("\n");
 
