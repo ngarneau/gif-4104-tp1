d550680c4e6fa1ddf3d6e93059525b054199dfaa
Nicolas Garneau <ngarneau@ngarneau.com>
2014-01-30 14:14:15 -0800

    Making it parallel

--- premiers.cpp
+++ premiers.cpp
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <pthread.h>
 #include "Chrono.hpp"
 
 unsigned long lMax = 1000;
 char *lFlags;
 const int sliceSize = 128*1024;
+
+pthread_mutex_t gMutex = PTHREAD_MUTEX_INITIALIZER;
+
+unsigned long gP = 2;
 
 // process only odd numbers of a specified block
 void eratosthenesBlockSimple(unsigned long from, unsigned long to)
@@ -46,21 +51,32 @@ void eratosthenesBlockSimple(unsigned long from, unsigned long to)
  * On va créer ici plusieurs block
  * séparer la charge de travail
  */
-void eratosthenesParBlock()
+void* eratosthenesParBlock(void* iArg)
 {
-    int numBlocks = 0;
     // each slices covers ["from" ... "to"], incl. "from" and "to"
-    for (unsigned long from = 2; from <= lMax; from += sliceSize)
+    do{
+        unsigned long to = gP + sliceSize;
+        if (to > lMax) {
+            to = lMax;
+        }
+        eratosthenesBlockSimple(gP, to);
+        pthread_mutex_lock(&gMutex);
+        gP+=sliceSize;
+        pthread_mutex_unlock(&gMutex);
+    }
+    while(gP <= lMax);
+
+    /*for (unsigned long from = 2; from <= lMax; from += sliceSize)
     {
         unsigned long to = from + sliceSize;
         if (to > lMax) {
             to = lMax;
         }
 
-        numBlocks++;
-
         eratosthenesBlockSimple(from, to);
-    }
+    }*/
+
+    pthread_exit(NULL);
 
     //printf("Number of blocks: %d\n", numBlocks);
 }
@@ -75,6 +91,12 @@ int main(int argc, char *argv[])
     // par défaut, prendre 1000
     if (argc >= 2) {
         lMax = atol(argv[1]);
+    }
+
+    // Déterminer le nombre de threads
+    int lThreads = 1; // par défault = 1
+    if (argc >= 3) {
+        lThreads = atoi(argv[2]);
     }
 
     // Démarrer le chronomètre
@@ -92,7 +114,16 @@ int main(int argc, char *argv[])
         lFlags[p]++;
     }
 
-    eratosthenesParBlock();
+    // create threads
+    pthread_t lIds[lThreads];
+    for(int i=1; i<=lThreads; ++i) {
+        pthread_create(&lIds[i-1], NULL, eratosthenesParBlock, NULL);
+    }
+
+    // wait for thread completion
+    for(int i=1; i<=lThreads; ++i) {
+        pthread_join(lIds[i-1], NULL);
+    }
 
     // Appliquer la passoire d'Ératosthène
     /*for (unsigned long p=3; p*p < lMax; p+=2) {
@@ -109,7 +140,7 @@ int main(int argc, char *argv[])
 
     // Afficher les nombres trouvés à la console
     for (unsigned long p=2; p<lMax; p++) {
-        if (lFlags[p] == 0) printf("%ld ", p);
+        //if (lFlags[p] == 0) printf("%ld ", p);
     }
     printf("\n");
 
